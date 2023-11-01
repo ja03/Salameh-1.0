2d3400d44bb3eaf2b625bb1d0dad55d7f993b0eb
Ahmad Mahmoud <abuhamdiehahmad@gmail.com>
2023-11-02 00:58:51 +0300

    sm-screen toggle list fixed

--- src/Components/hero-sub-comp/SideMenu.js
+++ src/Components/hero-sub-comp/SideMenu.js
@@ -4,15 +4,14 @@ const SideMenu = ({ links, onToggle, show }) => {
     return (
         <div
             className={`w-[30%] bg-cyan-700 h-screen fixed top-0 right-0 z-20 text-white transition-transform ease-in duration-300  
-        ${show ? "translate-x-0" : "translate-x-full"}`}>
+        ${show ? "translate-x-0" : "translate-x-full"} ${
+                show === false && "opacity-0"
+            }`}>
             <button className="z-50 m-2  p-1  text-lg" onClick={onToggle}>
                 <img src={xIcon} alt="close icon" />
             </button>
 
-            <ul
-                className={`flex flex-col items-start gap-4 py-2 ${
-                    show === false && "opacity-0"
-                }`}>
+            <ul className={`flex flex-col items-start gap-4 py-2`}>
                 {links.map((link, index) => (
                     <li
                         key={index}
